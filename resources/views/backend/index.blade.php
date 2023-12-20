69fc2d0cb7ae22dd8172436341119992da7f1634
esmail <120841700+hossainmdismail@users.noreply.github.com>
2023-12-20 14:42:33 +0600

    design

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -95,9 +95,7 @@
         ***********************************-->
         <div class="nav-header">
             <a href="index.html" class="brand-logo">
-                <img class="logo-abbr" src="{{ asset('backend') }}/images/logo.png" alt="/">
-                <img class="logo-compact" src="{{ asset('backend') }}/images/logo-text.png" alt="/">
-                <img class="brand-title" src="{{ asset('backend') }}/images/logo-text.png" alt="/">
+                <img class="logo-abbr" src="{{ asset('backend/icaniq.png') }}" alt="/">
             </a>
 
             <div class="nav-control">
