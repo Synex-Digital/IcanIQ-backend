444e4b8b5021a108699881d7952df0400dca5bee
esmail <120841700+hossainmdismail@users.noreply.github.com>
2023-12-21 10:11:17 +0600

    ssd

--- resources/views/backend/layouts/admin_login.blade.php
+++ resources/views/backend/layouts/admin_login.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('backend') }}/images/favicon.png">
-    <link href="{{ asset('backend') }}/css/style.css" rel="stylesheet">
+    {{-- <link href="{{ asset('backend') }}/css/style.css" rel="stylesheet"> --}}
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
         rel="stylesheet">
